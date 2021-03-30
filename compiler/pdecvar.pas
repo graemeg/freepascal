graemeg/freepascal

commit 1db9515569e89ee6143f41153df91d39fa5e2326

* instead of registering all and then deleting non-propgetter/setter procdefs in   pdecvar.read_property_dec, don't register them by default, and then only   register them, if they are propgetter/setter. This prevents dangling pointers   in current_module.deflist and potential use-after-free bugs.
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@48998 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/pdecvar.pas
@@ -360,8 +360,8 @@ implementation
          { Generate temp procdefs to search for matching read/write
            procedures. the readprocdef will store all definitions }
          paranr:=0;
-         readprocdef:=cprocdef.create(normal_function_level,true);
-         writeprocdef:=cprocdef.create(normal_function_level,true);
+         readprocdef:=cprocdef.create(normal_function_level,false);
+         writeprocdef:=cprocdef.create(normal_function_level,false);
 
          readprocdef.struct:=astruct;
          writeprocdef.struct:=astruct;
@@ -857,11 +857,14 @@ implementation
                message1(parser_e_implements_uses_non_implemented_interface,def.typename);
            until not try_to_consume(_COMMA);
 
-         { remove unneeded procdefs }
-         if readprocdef.proctypeoption<>potype_propgetter then
-           readprocdef.owner.deletedef(readprocdef);
-         if writeprocdef.proctypeoption<>potype_propsetter then
-           writeprocdef.owner.deletedef(writeprocdef);
+         { register propgetter and propsetter procdefs }
+         if assigned(current_module) and current_module.in_interface then
+           begin
+             if readprocdef.proctypeoption=potype_propgetter then
+               readprocdef.register_def;
+             if writeprocdef.proctypeoption=potype_propsetter then
+               writeprocdef.register_def;
+           end;
 
          result:=p;
       end;

commit 19c81bf9a0aff67aaa4ca2b97928f1e37832ca35

* fixed memory leak, introduced accidentally in r48998
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@49084 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/pdecvar.pas
@@ -861,9 +861,18 @@ implementation
          if assigned(current_module) and current_module.in_interface then
            begin
              if readprocdef.proctypeoption=potype_propgetter then
-               readprocdef.register_def;
+               readprocdef.register_def
+             else
+               readprocdef.free;
              if writeprocdef.proctypeoption=potype_propsetter then
-               writeprocdef.register_def;
+               writeprocdef.register_def
+             else
+               writeprocdef.free;
+           end
+         else
+           begin
+             readprocdef.free;
+             writeprocdef.free;
            end;
 
          result:=p;